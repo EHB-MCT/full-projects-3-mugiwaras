703f0508b4bfaa2d1ad13937d875c448f73d9992
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-01-30 22:08:04 +0100

    clearDBrequestForExhibitions

--- js/home.js
+++ js/home.js
@@ -4,14 +4,17 @@ window.onload = function () {
     fetchExhibitionData();
 
     function fetchExhibitionData() {
+        document.querySelector("#exhibitions").innerHTML = '<img src="images/Spinner-1s-200px.gif" alt="Loading">';
+
         fetch("http://localhost:3000/museumExhibitions")
             .then(response => response.json())
             .then(data => {
 
                 // Traitez les données reçues ici
                 // Par exemple, bouclez sur chaque objet dans le tableau de données et ajoutez-le à votre HTML
+                let exhibitionHTML = "";
                 data.forEach(function (exhibition) {
-                    let exhibitionHTML = `
+                    exhibitionHTML += `
               <div class="exhibition">
                 <img src="${exhibition.imagesrc}" alt="">
                 <div class="exhi-text">
@@ -21,11 +24,12 @@ window.onload = function () {
                 </div>
               </div>
             `;
-                    document.querySelector("#exhibitions").innerHTML += exhibitionHTML;
                 });
+                document.querySelector("#exhibitions").innerHTML = exhibitionHTML;
             })
             .catch(error => {
                 console.error("Error fetching exhibitions", error);
             });
     }
+
 };
